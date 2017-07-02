dd48404b63c9988c3b5ff4781945d822428f33ed
wangjiaxi <wangjiaxi@cnicg.cn>
2017-07-02 10:40:21 +0800

    fix permissoion and chinese

--- filer/admin/permissions.py
+++ filer/admin/permissions.py
@@ -8,13 +8,15 @@ from ..utils.compatibility import LTE_DJANGO_1_7
 
 
 class PrimitivePermissionAwareModelAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        # we don't have a "add" permission... but all adding is handled
-        # by special methods that go around these permissions anyway
-        # TODO: reactivate return False
-        return False
-
+    
+    # def has_add_permission(self, request, obj=None):
+    #     return False
+ 
     def has_change_permission(self, request, obj=None):
+        if obj and hasattr(request, 'user'):
+            if obj.owner != request.user and request.user.is_superuser is False:
+                return False
+
         if hasattr(obj, 'has_edit_permission'):
             if obj.has_edit_permission(request):
                 return True
